54ff8309911a318d51d2b6b7c3ec2c180e5c4a72
Lahatranomena <hei.lahatra.3@gmail.com>
2026-09-18 13:53:24 +0300

    feat: add new function Ranger

--- exercice2.js
+++ exercice2.js
@@ -8,4 +8,21 @@ function Echanger(tableau, premierIndex, deuxiemeIndex) {
 
 console.log(Echanger([0, 1, 0, 0, 0, 1], 0, 1));
 
+function Ranger(tableau) {
+    let nouveauTableau = []
+    let indexGauche = 0
+    let indexDroite = tableau.length-1
+    
+    for (let i = 0; i < tableau.length; i++) {
+        if (tableau[i] == 0) {
+            nouveauTableau[indexGauche] = tableau[i]
+            indexGauche++
+        } else {
+            nouveauTableau[indexDroite] = tableau[i]
+            indexDroite--
+        }
+    }
+    return nouveauTableau
+}
 
+console.log(Ranger([0, 1, 0, 0, 1, 0]));
